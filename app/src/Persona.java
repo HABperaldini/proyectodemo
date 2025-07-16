48355d01adc4c851c6b1225e86974226cf3d9fb7
stefano.peraldini <stefano.peraldini@hackaboss.com>
2025-07-16 19:08:56 +0200

    Implementada Persona

---
+++ app/src/Persona.java
@@ -0,0 +1,44 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String fechaDeNascimiento;
+
+    public Persona(int id, String nombre, String fechaDeNascimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaDeNascimiento = fechaDeNascimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaDeNascimiento() {
+        return fechaDeNascimiento;
+    }
+
+    public void setFechaDeNascimiento(String fechaDeNascimiento) {
+        this.fechaDeNascimiento = fechaDeNascimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", fechaDeNascimiento='" + fechaDeNascimiento + '\'' +
+                '}';
+    }
+}
